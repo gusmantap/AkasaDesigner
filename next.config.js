68cc8f44e6adcecff8842d07f8647d8238c50b43
gusmantap <bagus.mantonafi@gmail.com>
2022-02-05 12:24:38 +0800

    replace fabric

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
-// For building on vercel: https://github.com/Automattic/node-canvas/issues/1779
-module.exports = (phase, { defaultConfig }) => {
-  if (
-    process.env.LD_LIBRARY_PATH == null ||
-    !process.env.LD_LIBRARY_PATH.includes(
-      `${process.env.PWD}/node_modules/canvas/build/Release:`
-    )
-  ) {
-    process.env.LD_LIBRARY_PATH = `${
-      process.env.PWD
-    }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ""}`;
-  }
-};
+const config = {
+	webpack(config) {
+		config.resolve.alias['fabric'] = 'fabric-pure-browser'
+		return config;
+	}
+}
+
+module.exports = config;
